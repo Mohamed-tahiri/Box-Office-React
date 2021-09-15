e53d0d1d66736ae43f08dd9899f73a0244c3ade6
Mohamed-tahiri <mhdtahiri01@gmail.com>
2021-09-15 13:57:46 +0100

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import { Switch , Route } from 'react-router-dom';
 
 function App() {
-  return <div> h1 </div>
-
+  return (
+    <Switch>
+      <Route exact path="/"> 
+        This is home Page
+      </Route>
+      <Route exact path="/starred"> 
+        This is starred
+      </Route>
+      <Route> 
+        This is 404 page
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
